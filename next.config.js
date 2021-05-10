ed1f115b9c44d1b6c082a5102c089cfa8d6d31a8
沈慧慈 <lily7811245@kdanmobile.com>
2021-05-10 23:51:37 +0800

    [add]server

---
+++ next.config.js
@@ -0,0 +1,49 @@
+const webpack = require('webpack');
+
+const settings = {
+  webpack: (config) => {
+    config.module.rules.push(
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      },
+      {
+        test: /\.(txt|jpg|png|svg|webp)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              context: '',
+              emitFile: true,
+              name: '[path][name].[hash].[ext]',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg|webp)$/i,
+        use: {
+          loader: 'url-loader',
+          options: {
+            name: '/icons/[name].[ext]',
+            limit: 10240,
+          },
+        },
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader',
+      },
+    );
+
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        'process.env.ENV': JSON.stringify(process.env.ENV),
+        __CLIENT__: true,
+      }),
+    );
+    return config;
+  },
+};
+
+module.exports = settings;
